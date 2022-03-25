0c850df46496d9c3fdc657436a6f9e4504ae8e3c
suchita-bodke <102348477+suchita-bodke@users.noreply.github.com>
2022-03-25 13:25:34 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "state_http" {
+  name        = "state_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = "vpc-0846ee62b715a3392"
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
