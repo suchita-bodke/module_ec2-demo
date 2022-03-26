118320eabfe5ee9a3760ee323bdfbfd61737cd0f
suchita-bodke <102348477+suchita-bodke@users.noreply.github.com>
2022-03-26 12:21:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
+  count = "2"
   ami  = "ami-04893cdb768d0f9ee"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   subnet_id     = "subnet-0c5751b0a072871a9"
   key_name      = "linuxinstance"
   tags = {
